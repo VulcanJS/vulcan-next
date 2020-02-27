b7f37108a7ee09324004aed4556f73fd3c06b21b
withcloud <withclouddevelop@gmail.com>
2020-02-28 01:18:29 +0800

    temp update home page

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,74 @@
-import Head from 'next/head'
 import {
   useQuery,
   useMutation
 } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import { useForm } from 'react-hook-form'
+import { withVulcan } from '@vulcan/core'
+
+const IS_PROD = process.env.NODE_ENV === 'production'
+
+const graphqlUrl = IS_PROD
+  ? process.env.GRAPHQL_URL
+  : 'http://localhost:3001/graphql'
 
 const Home = () => {
-
-  const { loading, error, data } = useQuery(gql`
-  query current {
-    currentUser {
-      _id
+  const siteDataQuery = gql`
+    query getSiteData {
+      SiteData {
+        url
+        title
+        sourceVersion
+        logoUrl
+      }
     }
-  }
-  `)
-  const currentUser = data ? data.currentUser : null
+  `;
 
-  const [logout] = useMutation(gql`
-  mutation logout {
-    logout {
-      userId
-    }
-  }`)
+  const { data } = useQuery(siteDataQuery)
 
-  const [login, { data: loginData, error: loginError, loading: loginLoading }] = useMutation(gql`
-  mutation login($input: AuthPasswordInput) {
-    authenticateWithPassword(input: $input) {
-      userId
-      token
-    }
-  }`)
-  const { handleSubmit, register, errors } = useForm();
-  console.log('errors', errors)
-  const onSubmit = values => {
-    login({ variables: { input: { userSelector: { username: values.username }, password: values.password } } })
-  };
+  // const { error, data } = useQuery(gql`
+  //   query current {
+  //     currentUser {
+  //       _id
+  //     }
+  //   }
+  // `)
+  // const currentUser = data ? data.currentUser : null
 
-  const userId = currentUser
-    ? currentUser._id
-    : loginData
-      ? loginData.authenticateWithPassword.userId
-      : null
+  // const [logout] = useMutation(gql`
+  //   mutation logout {
+  //     logout {
+  //       userId
+  //     }
+  //   }
+  // `)
+
+  // const [login, { data: loginData }] = useMutation(gql`
+  //   mutation login($input: AuthPasswordInput) {
+  //     authenticateWithPassword(input: $input) {
+  //       userId
+  //       token
+  //     }
+  //   }
+  // `)
+
+  // const { handleSubmit, register, errors } = useForm();
+  // console.log('errors', errors)
+  // const onSubmit = values => {
+  //   login({ variables: { input: { userSelector: { username: values.username }, password: values.password } } })
+  // };
+
+  // const userId = currentUser
+  //   ? currentUser._id
+  //   : loginData
+  //     ? loginData.authenticateWithPassword.userId
+  //     : null
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        {JSON.stringify(data)}
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        {!!error && (
+        {/* {!!error && (
           <div>
             <p>Could not fetch current user!</p>
           </div>
@@ -69,12 +79,14 @@ const Home = () => {
             <div>Store this token into the `meteor_login_token` cookie or pass it as the Authorization header to make authorized queries.</div>
           </div>
         )}
-        {userId ? (
-          <div>
-            You are logged in from a Vulcan application as {userId}
-            <button onClick={() => logout()}>Logout</button>
-          </div>
-        ) : (
+        {userId
+          ? (
+            <div>
+              You are logged in from a Vulcan application as {userId}
+              <button onClick={() => logout()}>Logout</button>
+            </div>
+          )
+          :(
             <div>
               <form onSubmit={handleSubmit(onSubmit)}>
                 <input
@@ -93,191 +105,11 @@ const Home = () => {
                 <button type="submit">Login using a Vulcan app</button>
               </form>
             </div>
-          )}
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-          </a>
-        </div>
+          )
+        } */}
       </main>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-      <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
     </div>
   )
 }
 
-export default Home
+export default withVulcan({ ssr: true, graphqlUrl })(Home)
