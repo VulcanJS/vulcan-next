66be1fccfbe4bfefaf0a888f21a9a0ae68b79242
eric-burel <eb@lebrun-burel.com>
2020-06-04 09:07:06 +0200

    removed multi-env-demo import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { AppProps } from 'next/app';
-import { withVulcan } from '@vulcan/core';
-import environment from '@vulcan/multi-env-demo';
+import { AppProps } from "next/app";
+import { withVulcan } from "@vulcan/core";
+// import environment from '@vulcan/multi-env-demo';
 
-console.log('imported environment', environment); // should display "server"/"client" depending on the environment, just a test
+// console.log('imported environment', environment); // should display "server"/"client" depending on the environment, just a test
 
-const IS_PROD = process.env.NODE_ENV === 'production';
+const IS_PROD = process.env.NODE_ENV === "production";
 
 const graphqlUrl = IS_PROD
   ? process.env.GRAPHQL_URL
-  : 'http://localhost:3001/graphql';
+  : "http://localhost:3001/graphql";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
