ff923196398c8536f785dcd931910dd1c559e731
eric-burel <eb@lebrun-burel.com>
2020-06-09 19:58:56 +0200

    failing test for ssr

---
+++ cypress/integration/vns/ssr.spec.ts
@@ -0,0 +1,7 @@
+describe("ssr", () => {
+    it.skip("does not server-side render in loading state (either error or data)", () => {
+        cy.visit("/vns/ssrDebug")
+        // TODO: test is slower because it will retry the should continuously, did not find an option to avoid this in should
+        cy.contains("loading").should("not.exist")
+    })
+});
---
+++ src/pages/vns/ssrDebug.tsx
@@ -0,0 +1,34 @@
+import { useQuery /*, useMutation*/ } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+//import { useForm } from "react-hook-form";
+
+const SsrDebugPage = () => {
+    const vulcanSiteDataQuery = gql`
+    query getSiteData {
+      SiteData {
+        url
+        title
+        sourceVersion
+        logoUrl
+      }
+    }
+  `;
+
+    const { data, loading, error } = useQuery(vulcanSiteDataQuery);
+
+    let content = "";
+    if (loading) {
+        content = "loading"; // on ${client.name}`
+    } else if (error) {
+        content = "error"
+    } else if (data) {
+        content = "data"
+    }
+    return (
+        <div>
+            {content}
+        </div>
+    );
+};
+
+export default SsrDebugPage;
