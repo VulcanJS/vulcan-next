b33bed65822a63f5e278252bc2272d3a7a9cc2c4
eric-burel <eb@lebrun-burel.com>
2020-06-09 12:42:57 +0200

    install of storybook 6, first test without Next import

---
+++ .storybook/main.js
@@ -0,0 +1,7 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.@(js|ts|jsx|tsx)",
+    "../src/**/*.stories.@(js|ts|jsx|tsx)",
+  ],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+};
