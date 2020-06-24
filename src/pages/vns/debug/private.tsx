663bcc1cf5a6cb72cb6132900f99a3e499ac56e4
eric-burel <eb@lebrun-burel.com>
2020-06-24 19:43:37 +0200

    fix redirect in static mode and client-side

--- src/pages/vns/debug/private.tsx
+++ src/pages/vns/debug/private.tsx
@@ -1,7 +1,8 @@
 /**
- * Demo an authenticated page
- * @see https://spectrum.chat/next-js/general/redirect-the-right-way~6d1b6631-340d-4de3-9127-8cf08ec02bcc
- * @see https://jasonraimondi.com/posts/secure-a-next-js-application-with-jwt-and-a-private-route-higher-order-component/#add-a-_privateroute_-higher-order-component-hoc-to-secure-the-app-from-unauthorized-access
+ * Demo a private page
+ *
+ * @see https://github.com/VulcanJS/vulcan-next-starter/issues/49
+ * @see https://github.com/vercel/next.js/discussions/14531
  */
 import Link from "next/link";
 import { NextPage, NextPageContext } from "next";
@@ -12,7 +13,7 @@ import {
   isStaticExportCtx,
 } from "@vulcan/next-utils";
 import debug from "debug";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const debugNext = debug("vns:next");
 
 // @ssr-only
@@ -21,28 +22,57 @@ export const redirectServer = (ctx: NextPageContext) => (pathname: string) => {
   ctx.res.end();
 };
 
+/**
+ * @client-only
+ * Your logic to check if user is allowed, client-side
+ *
+ * Here we use a query param, in real life you might want to check
+ * the presence of a cookie or a token in the localStorage
+ * NOTE: router.query is sometimes empty, henve the URLSearchParams
+ */
+const isAllowedClient = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return !!urlParams.get("allowed");
+};
+
 interface PrivatePageProps {
   isStaticExport?: boolean;
   isServerRender?: boolean;
   isAllowedDuringSSR?: boolean;
 }
 const PrivatePage: NextPage<PrivatePageProps> = (props) => {
-  // SCENARIO 1: handle redirection purely client-side after static export
+  // SCENARIO 1: handle redirection and rendering purely client-side, after static export or during a client-side redirect
   const router = useRouter();
+  const [isAllowedState, setAllowedState] = useState(false); // use state to avoid the flash
+  const { isAllowedDuringSSR } = props;
   useEffect(() => {
-    if (!router.query.allowed) {
-      debugNext("Redirecting client-side", "router.query:", router.query);
+    const isAllowed = isAllowedClient();
+    if (!isAllowed) {
+      debugNext("Redirecting client-side");
       router.push("/vns/debug/public");
+    } else {
+      setAllowedState(true);
     }
-  });
-  // Renders nothing in this this case
+  }, [router]);
+  // SCENARIO 1.1: static export (rendering server side at build time)
   const { isStaticExport } = props;
-  if (isStaticExport) {
+  if (isStaticExport && !isClientRender()) {
     debugNext(
-      "We render nothing during static export, this is a private page (only rendered client side)"
+      "We render nothing during static export, server side this is a private page (only rendered client side)"
     );
     return <></>;
+
+    // SCNEARIO 1.2: client render
+    // SCENARIO 1.2.1 : client render after a server render (we know if user is allowed thanks to initialProps)
+    // and 1.2.2: client render after a client redirect (we must wait for the useEffect to run)
+  } else if (isClientRender() && !(isAllowedDuringSSR || isAllowedState)) {
+    debugNext(
+      "We render nothing if user is not allowed or a redirect is happening or we simply wait for the effect to run"
+    );
+    // we render nothing when waiting for a redirect or to check that we are allowed or not being auth (avoids a flash)
+    return <></>;
   }
+  debugNext("Rendering private page");
 
   return (
     <>
