8dd7dbbf00798c7084ad448155cf639baab1ba4c
eric-burel <eb@lebrun-burel.com>
2020-07-22 15:45:32 +0200

    update welcome message for storybook

--- stories/0-Welcome.stories.js
+++ stories/0-Welcome.stories.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { linkTo } from '@storybook/addon-links';
-import { Welcome } from '@storybook/react/demo';
+import React from "react";
+// import { linkTo } from "@storybook/addon-links";
+import { Welcome } from "@storybook/react/demo";
+import { Typography } from "@material-ui/core";
 
 export default {
-  title: 'Welcome',
+  title: "Welcome",
   component: Welcome,
 };
 
-export const ToStorybook = () => <Welcome showApp={linkTo('Button')} />;
+export const ToStorybook = () => {
+  return (
+    <div>
+      <Typography variant="h2"> Welcome to Storybook - VNS Edition</Typography>
+      <Typography variant="body1">
+        Explore existing stories using the left menu, or{" "}
+        <a
+          href="https://storybook.js.org/docs/basics/writing-stories/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          read the docs and learn to write your own stories.
+        </a>
+      </Typography>
+    </div>
+  );
+
+  //<Welcome showApp={linkTo('Button')} />;
+};
 
 ToStorybook.story = {
-  name: 'to Storybook',
+  name: "to Storybook",
 };
