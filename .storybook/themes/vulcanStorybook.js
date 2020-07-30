f01d98fb6b20a92ab0fc9df5bae1e969cf63121b
eric-burel <eb@lebrun-burel.com>
2020-07-30 15:57:18 +0200

    tweaking storybook theme

--- .storybook/themes/vulcanStorybook.js
+++ .storybook/themes/vulcanStorybook.js
@@ -2,4 +2,8 @@ import { create } from "@storybook/theming/create";
 
 export default create({
   colorPrimary: "#E45729",
+  colorSecondary: "#3F77FA",
+  appBorderColor: "#61dafbaa",
+  brandTitle: "Storybook -VNS Edition",
+  brandImage: "/img/vns-logo-96.png",
 });
