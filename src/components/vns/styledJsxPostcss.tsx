d51b4575a082bb4b87ccdcbff2e0751a7928871e
eric-burel <eb@lebrun-burel.com>
2020-11-20 09:52:38 +0100

    fix component

--- src/components/vns/styledJsxPostcss.tsx
+++ src/components/vns/styledJsxPostcss.tsx
@@ -1,4 +1,4 @@
-export const StPostCSS () => (
+export const StPostCSS = () => (
   <div>
     <div className="yellow">
       <h1>I am yellow</h1>
@@ -14,4 +14,4 @@ export const StPostCSS () => (
     </style>
   </div>
 );
-export default StPostCSS
+export default StPostCSS;
