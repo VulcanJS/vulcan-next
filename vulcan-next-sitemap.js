39555b66c2f8607c6c84004a03273aab9f3ec954
Neobii <iwilldevelopcodeforyou@gmail.com>
2021-02-04 02:23:31 -0600

    added sitemap

---
+++ vulcan-next-sitemap.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteUrl: 'https://vulcan.next',
+  generateRobotsTxt: true,
+  //outDir: './out/', for static builds
+  exclude: ['/vns/*', '/login', '/profile', '/signup', '/admin', '/auth'],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/vns/*', '/login', '/profile', '/signup', '/admin', '/auth'],
+      },
+    ]
+  }
+}
