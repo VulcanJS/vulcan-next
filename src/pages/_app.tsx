1bb46ff6e74d5efcdb300cc7d0265cc3e9df3a73
eric-burel <eb@lebrun-burel.com>
2021-03-30 18:34:29 +0200

    added favicon related stuff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -35,6 +35,32 @@ function VNApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        {/* Favicon created using https://realfavicongenerator.net/ */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color="#5bbad5"
+        ></link>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <MuiThemeProvider>
         <SCThemeProvider>
