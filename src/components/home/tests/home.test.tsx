da73e6d2d42f879ed3287a4569c35ed6ca67628f
Timi Duban <ti.mi.34@hotmail.fr>
2021-06-15 12:41:56 +0200

    add react-testing

--- src/components/home/tests/home.test.tsx
+++ src/components/home/tests/home.test.tsx
@@ -1,8 +1,31 @@
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  act,
+} from "@testing-library/react";
+// @see https://storybook.js.org/addons/@storybook/react-testing
+import { composeStories } from "@storybook/testing-react";
+// import all stories from the stories file
+import * as stories from "../stories/home.stories";
 
+// Every component that is returned maps 1:1 with the stories, but they already contain all decorators from story level, meta level and global level.
+const { HomeStory } = composeStories(stories);
+
+describe('components/home', () => {
+  test("renders home with testing-react", () => {
+    const { queryAllByText } = render(<HomeStory />);
+    expect(queryAllByText("Vulcan Next", { exact: false })).not.toBeNull();
+    expect(
+      queryAllByText("Vulcan Next", { exact: false }).length
+    ).toBeGreaterThan(0);
+  });
+});
+
+/* Old way - without testing-react
 import Home from "../home";
-
 describe("components/home", () => {
   test("renders home with React Testing", () => {
     const { queryAllByText } = render(<Home />);
@@ -11,4 +34,4 @@ describe("components/home", () => {
       queryAllByText("Vulcan Next", { exact: false }).length
     ).toBeGreaterThan(0);
   });
-});
+}); */
