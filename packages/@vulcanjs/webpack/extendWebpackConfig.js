bd1de6527e433cccb06ca977e99b2f0a5894ec38
eric-burel <eb@lebrun-burel.com>
2021-09-28 14:39:01 +0200

    add stream to fallback

--- packages/@vulcanjs/webpack/extendWebpackConfig.js
+++ packages/@vulcanjs/webpack/extendWebpackConfig.js
@@ -59,7 +59,11 @@ const extendWebpackConfig = (environment) => (webpackConfig) => {
   // @see https://github.com/angular/angular-cli/issues/20819
   // TODO: this is the sign of a server code leak, investigate if still needed
   // after we get rid of those leaks (O9/2021)
-  webpackConfig.resolve.fallback = { events: false, path: false };
+  webpackConfig.resolve.fallback = {
+    events: false,
+    path: false,
+    stream: false,
+  };
 
   withMagicImports(webpackConfig);
   return webpackConfig;
