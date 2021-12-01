423989e89bcc34f7733a0fcaacb27900707ff35e
eric-burel <eb@lebrun-burel.com>
2021-12-01 18:28:59 +0100

    add github star and fork buttons

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -1,5 +1,6 @@
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import { ReactElement } from "react";
+import Head from "next/head";
 
 /**
  * Default layout, to be used in pages
@@ -10,7 +11,33 @@ interface PageLayoutProps {
 }
 
 const PageLayout = ({ children }: PageLayoutProps) => (
-  <Container sx={{ mx: "auto", my: 2 }}>{children}</Container>
+  <Container sx={{ mx: "auto", my: 2 }}>
+    <Head>
+      <script async defer src="https://buttons.github.io/buttons.js"></script>
+    </Head>
+
+    <header>
+      <Box sx={{ textAlign: "center" }}>
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=VulcanJS&repo=vulcan-next&type=star&count=true&size=large"
+          frameBorder="0"
+          scrolling="0"
+          width="170"
+          height="30"
+          title="GitHub"
+        ></iframe>
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=VulcanJS&repo=vulcan-next&type=fork&count=true&size=large"
+          frameBorder="0"
+          scrolling="0"
+          width="170"
+          height="30"
+          title="GitHub"
+        ></iframe>
+      </Box>
+    </header>
+    {children}
+  </Container>
 );
 
 /**
