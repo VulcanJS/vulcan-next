19221e39c20aad642aa32c2d047ee1fc2f46622e
eric-burel <eb@lebrun-burel.com>
2021-12-01 19:29:31 +0100

    add warning for SMTP setup during tests

--- src/lib/api/mail/transports.ts
+++ src/lib/api/mail/transports.ts
@@ -16,6 +16,11 @@ if (process.env.SMTP_HOST) {
     port: process.env.SMTP_PORT,
     secure: !!process.env.SMTP_SECURE, // true for 465, false for other ports
   };
+} else {
+  console.warn("SMTP Transport not set during development");
+  console.warn(
+    "If you run auth test using Cypress, use 'yarn run dev:test' instead!"
+  );
 }
 
 export const localMailTransport = nodemailer.createTransport(transport);
