7f963082b1635a7c68898305bdc4e2febe75f11e
eric-burel <eb@lebrun-burel.com>
2022-03-12 12:03:44 +0100

    fix test unit babel config

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ const commonConfig = {
   // transform: undefined,
   transform: {
     //"^.+\\.[jt]sx?$": "ts-jest",
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.(js|jsx|ts|tsx|mjs)$": "<rootDir>/node_modules/babel-jest",
     // MDX support
     "^.+\\.(md|mdx)$": "jest-transformer-mdx",
   },
@@ -39,14 +39,14 @@ const commonConfig = {
     ],
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
 
     // Handle CSS imports (without CSS modules)
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
 
     // Handle image imports
     // https://jestjs.io/docs/webpack#handling-static-assets
-    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`,
+    "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
   },
 
   // The directory where Jest should output its coverage files
