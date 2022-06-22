e8fbfda57a5df7fbd3feb3df640c30e0d2eac80c
eric-burel <eb@lebrun-burel.com>
2022-06-22 18:23:13 +0200

    use MongoId where relevant

--- src/core/models.server.ts
+++ src/core/models.server.ts
@@ -7,6 +7,9 @@ const models = [User, SampleModel];
 // Add default connectors and dataSources creators for models that may miss some
 // @see https://www.apollographql.com/docs/apollo-server/data/data-sources
 import { addDefaultMongoConnector } from "@vulcanjs/mongo-apollo/server";
-addDefaultMongoConnector(models);
+addDefaultMongoConnector(
+  models
+  /* {useStringId: true} // Uncomment to use String _ids in Mongo:*/
+);
 
 export default models;
